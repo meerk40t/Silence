ab12100b1fbcae1802207b277280b260f56978ae
Tatarize <tatarize@gmail.com>
2021-03-31 18:27:46 -0700

    Drop Requirement svgelements

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import setup
 setup(
     install_requires=[
         "pyusb>=1.0.0",
-        "svgelements",
     ],
     extras_require={
         'all': ['wxPython>=4.0.0', "Pillow>=7.0.0", "ezdxf>=0.13.0"],
